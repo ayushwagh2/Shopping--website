c7aa2327afc632dd167ada244773864e15d60f67
Ayush <ayushwagh2002@gmail.com>
2024-10-08 21:43:19 +0530

    1st
    First Commit

---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Iphone",
+        price: 999,
+        productImage: product1,
+    },
+    {
+        id:2,
+        productName: "prodct2",
+        price: 999,
+        productImage: product2,
+    },
+    {
+        id:3,
+        productName: "prodct3",
+        price: 999,
+        productImage: product3,
+    },
+    {
+        id:4,
+        productName: "prodct4",
+        price: 999,
+        productImage: product4,
+    },
+    {
+        id:5,
+        productName: "prodct5",
+        price: 999,
+        productImage: product5,
+    },
+    {
+        id:6,
+        productName: "prodct6",
+        price: 999,
+        productImage: product6,
+    },
+    {
+        id:7,
+        productName: "prodct7",
+        price: 999,
+        productImage: product7,
+    },
+    {
+        id:8,
+        productName: "prodct8",
+        price: 999,
+        productImage: product8,
+    },
+     
+]
